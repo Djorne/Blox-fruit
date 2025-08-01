b791ce964341f9d5325983975936215afc600826
Djorne <Djornebruggink@gmail.com>
2025-08-01 17:10:22 +0200

    Update and rename luarmorloader.lua to luarmorloader.luo

--- luarmorloader.lua
+++
@@ -1,9 +0,0 @@
--- 🔧 Instellingen
-getgenv().Team = "Pirates"
-getgenv().AutoLoad = false -- Laadt script bij server hop
-getgenv().SlowLoadUi = false
-getgenv().ForceUseSilentAimDashModifier = false -- Forceer silent aim
-getgenv().ForceUseWalkSpeedModifier = false -- Forceer walkspeed
-
--- 🚀 Laad Luarmor script
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
